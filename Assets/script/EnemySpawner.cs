07d8810032c64b745fad21c80d75891e8314a8f2
roulios44 <jules.croizier@gmail.com>
2023-03-20 10:49:44 +0100

    changing enemySpawner and add pause when in shop

--- Assets/script/EnemySpawner.cs
+++ Assets/script/EnemySpawner.cs
@@ -16,7 +16,7 @@ public class EnemySpawner : MonoBehaviour
     private IEnumerator spawnEnemies(float interval, GameObject enemy)
     {
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5f), Random.Range(-6f, 6f), 0), Quaternion.identity);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(this.transform.position.x,this.transform.position.y, 0), Quaternion.identity);
         StartCoroutine(spawnEnemies(interval, enemy));
     }
 }
