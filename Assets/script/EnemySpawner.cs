59a0136ff2fdb6d87f87d8f58eaf0752caa4c271
roulios44 <jules.croizier@gmail.com>
2023-04-03 14:06:05 +0200

    managin spawneur

--- Assets/script/EnemySpawner.cs
+++ Assets/script/EnemySpawner.cs
@@ -6,17 +6,17 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField]
     private GameObject humanPrefab;
-    private float humanSpawnInterval = 3.0f;
+    private float humanSpawnInterval = 9.0f;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(spawnEnemies(humanSpawnInterval, humanPrefab));
+        StartCoroutine(SpawnEnemies(humanSpawnInterval, humanPrefab));
     }
 
-    private IEnumerator spawnEnemies(float interval, GameObject enemy)
+    private IEnumerator SpawnEnemies(float interval, GameObject enemy)
     {
         yield return new WaitForSeconds(interval);
         GameObject newEnemy = Instantiate(enemy, new Vector3(this.transform.position.x,this.transform.position.y, 0), Quaternion.identity);
-        StartCoroutine(spawnEnemies(interval, enemy));
+        StartCoroutine(SpawnEnemies(interval, enemy));
     }
 }
